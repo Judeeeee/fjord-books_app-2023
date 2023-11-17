7c64cacd0a92a4aed2a9c8d16dcac4f99e2bfe78
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-11-17 18:26:28 +0900

    アソシエーションを定義

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,2 +1,4 @@
 class Mention < ApplicationRecord
+  belongs_to :mentioned
+  belongs_to :mentioning
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,9 +3,12 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :mentioneds, class_name: "mentions", foreign_key: :mentioned_report_id, dependent: :destroy
+  has_many :mentionings, class_name: "mentions", foreign_key: :mentioning_report_id, dependent: :destroy
 
   validates :title, presence: true
   validates :content, presence: true
+  validates_uniqueness_of :mentioned_report, scope: :mentioning_report
 
   def editable?(target_user)
     user == target_user
