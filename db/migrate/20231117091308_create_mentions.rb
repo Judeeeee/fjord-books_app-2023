98fd7238effc4ec75e2fc0a0f45a6291d623f066
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-11-17 21:28:14 +0900

    指定を間違えていたため修正

--- db/migrate/20231117091308_create_mentions.rb
+++ db/migrate/20231117091308_create_mentions.rb
@@ -1,10 +1,10 @@
 class CreateMentions < ActiveRecord::Migration[7.0]
   def change
     create_table :mentions do |t|
-      t.string :mentioned_report_id, null: false, foreign_key: true
-      t.string :mentioning_report_id, null: false, foreign_key: true
+      t.integer :mentioned_report_id, null: false, foreign_key: true
+      t.integer :mentioning_report_id, null: false, foreign_key: true
       t.timestamps
-      add_index :mentions, [:mentioned_report, :mentioning_report], unique: true
+      t.index :mentions, [:mentioned_report, :mentioning_report], unique: true
     end
   end
 end
