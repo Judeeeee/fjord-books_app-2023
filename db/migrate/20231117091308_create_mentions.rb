3f2ad8dbead1301264bdffad2868200c2ffde97e
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-11-17 18:20:46 +0900

    外部キー制約とNOT NULL制約を付与

--- db/migrate/20231117091308_create_mentions.rb
+++ db/migrate/20231117091308_create_mentions.rb
@@ -1,8 +1,8 @@
 class CreateMentions < ActiveRecord::Migration[7.0]
   def change
     create_table :mentions do |t|
-      t.string :mentioned_report_id
-      t.string :mentioning_report_id
+      t.string :mentioned_report_id, null: false, foreign_key: true
+      t.string :mentioning_report_id, null: false, foreign_key: true
       t.timestamps
       add_index :mentions, [:mentioned_report, :mentioning_report], unique: true
     end
