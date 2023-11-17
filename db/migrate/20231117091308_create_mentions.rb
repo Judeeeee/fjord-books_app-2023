9aecfffa0a792e6547ef1f12730bb582a71a8f43
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-11-17 18:18:17 +0900

    組み合わせを一意にするためにmigrationファイルに追記

--- db/migrate/20231117091308_create_mentions.rb
+++ db/migrate/20231117091308_create_mentions.rb
@@ -3,8 +3,8 @@ class CreateMentions < ActiveRecord::Migration[7.0]
     create_table :mentions do |t|
       t.string :mentioned_report_id
       t.string :mentioning_report_id
-
       t.timestamps
+      add_index :mentions, [:mentioned_report, :mentioning_report], unique: true
     end
   end
 end
