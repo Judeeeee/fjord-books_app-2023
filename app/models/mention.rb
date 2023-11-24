360de55530015b6927585f8bb3c133786a9bc4f8
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-11-24 17:23:43 +0900

    sizeだと両配列が同じサイズの時に条件分岐に入らないので、レコード更新・削除を配列の比較で実装

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -13,14 +13,29 @@ class Mention < ApplicationRecord
   def self.delete_mentions(report)
     # 中間テーブルに保存されているレコードから言及先の日報IDを取得する。
     mentioned_report_ids = Mention.where(mentioned_id: report.id).map{|mention| mention.mentioning_id}
-    delete_taeget_reports = mentioned_report_ids - report.mentioning
-    delete_taeget_reports.each do |delete_taeget_report|
-      report = Mention.where(mentioned_id: report.id, mentioning: delete_taeget_report).first.id
-      Mention.delete(report)
+
+    # 配列サイズが違っていたら削除、同じだったら更新
+    if mentioned_report_ids.size == report.mentioning_report_links
+      # 更新
+      mentioned_report_ids.each do |mentioned_report_id|
+        update_target_report = Mention.where(mentioned_id: report.id, mentioning: mentioned_report_id).first.id
+        report.mentioning_report_links.each do |mentioning_report_link|
+          update_target_report.update(mentioning_id: mentioning_report_link)
+        end
+      end
+    else
+      # 削除
+      delete_taeget_reports = mentioned_report_ids - report.mentioning_report_links
+      delete_taeget_reports.each do |delete_taeget_report|
+        report = Mention.where(mentioned_id: report.id, mentioning: delete_taeget_report).first.id
+        Mention.delete(report)
+      end
     end
   end
 
   def self.links_update?(report)
-    self.where(mentioned_id: report.id).size > report.mentioning_report_links.size
+    # 中間テーブルのレコードと本文のリンクに違いがあるときにtrueを返したい
+    mentioned_report_ids = self.where(mentioned_id: report.id).map{|mention| mention.mentioning_id}
+    mentioned_report_ids != (report.mentioning_report_links)
   end
 end
