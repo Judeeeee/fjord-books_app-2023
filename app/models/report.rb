896787b21f5c18a992cc4d5b86ad1cd0bb6a7987
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-11-28 14:47:56 +0900

    report_linksの構造的にflat_mapは使えない

--- app/models/report.rb
+++ app/models/report.rb
@@ -19,7 +19,6 @@ class Report < ApplicationRecord
 
   def mentioning_report_links
     report_links = content.scan(%r{http://localhost:3000/reports/(\d+)})
-    # NOTE: flatten.map(&:to_i) => flat_map(&:to_i) にできるかも
     report_links.flatten.map(&:to_i)
   end
 
