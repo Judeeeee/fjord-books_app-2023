3d3debfc1a6f0d1f4d03e61f99dad0f32228587e
Judeeeee <Judeee.pg.zen1@gmail.com>
2023-12-19 14:32:20 +0900

    has_many throughを使用する方法へ変更

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,7 +4,10 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :mentioneds, class_name: 'Mention', foreign_key: :mentioned_id, dependent: :destroy, inverse_of: :mentioned
-  has_many :mentionings, class_name: 'Mention', foreign_key: :mentioning_id, dependent: :destroy, inverse_of: :mentioned
+  has_many :mentionings, class_name: 'Mention', foreign_key: :mentioning_id, dependent: :destroy, inverse_of: :mentioning
+
+  has_many :mentioning_reports, through: :mentionings, source: :mentioned
+  has_many :mentioned_reports, through: :mentioneds, source: :mentioning
 
   validates :title, presence: true
   validates :content, presence: true
