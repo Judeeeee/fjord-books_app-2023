5bcb0b3dd5e450909d824ef95ab0499356e27a6f
Judeeeee <Judeee.pg.zen1@gmail.com>
2024-02-02 12:23:26 +0900

    日報更新時(言及の追加、維持、削除)の動作確認

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -26,4 +26,25 @@ class ReportTest < ActiveSupport::TestCase
     expect = 1
     assert_equal expect, link_included_report.mentioning_report_ids[0]
   end
+
+  test '他日報のリンクが本文に存在する日報に、更に他日報のリンクを追加し更新する' do
+    mentioning_reports = reports(:mentioning_report)
+    mentioning_reports.update(title: 'add mentioning report link', content: 'http://localhost:3000/reports/1, http://localhost:3000/reports/2')
+    expect = [1, 2]
+    assert_equal expect, mentioning_reports.mentioning_report_ids
+  end
+
+  test '他日報のリンクが存在する本文を変更せずに更新する' do
+    mentioning_reports = reports(:mentioning_report)
+    mentioning_reports.update(title: 'maintain report content', content: 'http://localhost:3000/reports/2')
+    expect = [2]
+    assert_equal expect, mentioning_reports.mentioning_report_ids
+  end
+
+  test '他日報のリンクが存在する本文からリンクを削除し更新する' do
+    mentioning_reports = reports(:mentioning_report)
+    mentioning_reports.update(title: 'delete mentioning report link', content: '言及の削除')
+    expect = []
+    assert_equal expect, mentioning_reports.mentioning_report_ids
+  end
 end
